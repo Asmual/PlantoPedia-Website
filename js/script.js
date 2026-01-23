5ee629499985503251b4100a638516612dd02d25
Asmual <Asmual04@gmail.com>
2026-01-23 21:48:30 +0600

    PlantoPedia Website Work Start

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar_toggle = document.querySelector('.navbar_toggle');
+  const header_menu = document.querySelector('.header_menu');
+
+  navbar_toggle.addEventListener('click', () => {
+    navbar_toggle.classList.toggle('active');
+    header_menu.classList.toggle('active');
+  });
+});
