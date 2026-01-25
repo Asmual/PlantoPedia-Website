34e5c9ccbcb101532a340497079e3a291d0189ab
Asmual <Asmual04@gmail.com>
2026-01-25 22:13:27 +0600

    JavaScript Added Search function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const navbar_toggle = document.querySelector('.navbar_toggle');
+//     const header_menu = document.querySelector('.header_menu');
+
+//     navbar_toggle.addEventListener('click', () => {
+//         navbar_toggle.classList.toggle('active');
+//         header_menu.classList.toggle('active');
+//     });
+// });
+//
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const navbar_toggle = document.querySelector('.navbar_toggle');
     const header_menu = document.querySelector('.header_menu');
@@ -7,3 +21,23 @@ document.addEventListener('DOMContentLoaded', () => {
         header_menu.classList.toggle('active');
     });
 });
+// 
+/* ===== Flower Search System ===== */
+    const searchInput = document.querySelector('.search_input');
+    const flowerCards = document.querySelectorAll('.flower_card');
+
+    searchInput.addEventListener('keyup', () => {
+        const searchValue = searchInput.value.toLowerCase();
+
+        flowerCards.forEach(card => {
+            const flowerName = card.querySelector('h3').innerText.toLowerCase();
+
+            if (flowerName.includes(searchValue)) {
+                card.style.display = "flex";
+            } else {
+                card.style.display = "none";
+            }
+        
+
+        });
+    });
